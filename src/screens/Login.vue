5bb6487c62df50f5cf7caa22ce35bff61cb15266
Nil Valero <nvalero13@gmail.com>
2022-12-20 21:10:45 +0100

    add login

--- src/screens/Login.vue
+++ src/screens/Login.vue
@@ -2,13 +2,14 @@
     <div class="flex justify-center mx-auto w-[1200px]">
         <div class="rounded-sm shadow-lg bg-gray-100 w-[400px] p-5 z-10">
             <h1 class="text-emerald-500 text-2xl font-bold uppercase">Login</h1>
-            <form action="" class="mt-5 mb-10">
+            <form @submit.prevent="login" class="mt-5 mb-10">
                 <label for="Email" class="text-emerald-300 block my-2">Email</label>
-                <input type="text" name="Email" class="w-full p-2 rounded-sm outline-none">
+                <input v-model="email" type="text" name="Email" class="w-full p-2 rounded-sm outline-none">
                 <label for="Password" class="text-emerald-300 block my-2">Password</label>
-                <input type="password" name="Password" class="w-full p-2 rounded-sm outline-none">
+                <input v-model="password" type="password" name="Password" class="w-full p-2 rounded-sm outline-none">
 
                 <input type="submit" value="Login" class="block bg-emerald-500 text-white px-5 py-2 my-3 w-full hover:bg-emerald-600 pointer">
+                <div v-show="errorMsg" class="w-full bg-red-400 text-white text-center mt-3 p-2">{{ errorMsg }}</div>
             </form>
             <p class="text-center my-5">Don't have an account? <router-link class="text-emerald-500 hover:underline" to="/register">Register</router-link></p>
         </div>
@@ -21,9 +22,28 @@
 <script setup>
 import { ref } from 'vue';
 import { supabase } from '../supabase';
+import { useRouter } from 'vue-router';
 
+const email = ref(null);
+const password = ref(null);
+const errorMsg = ref(null);
+const router = useRouter();
     
-
+const login = async () => {
+    try {
+        const {error} = await supabase.auth.signIn({
+            email: email.value,
+            password: password.value
+        });
+        if (error) throw error;
+        router.push({name: "Home"})
+    } catch(error) {
+        errorMsg.value = `Error: ${error.message}`
+        setTimeout(() => {
+            errorMsg.value = null
+        }, 5000);
+    }
+}
 
 </script>
 
